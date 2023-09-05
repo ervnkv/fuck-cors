1fc4d4c755ce1b82b9b178e5923652c8faa06f88
Revenkov Egor <revenkov.egor@gmail.com>
2023-09-05 23:11:53 +0300

    Fix api v1

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,20 +1,30 @@
-const axios = require('axios');
+import axios from 'axios';
 
-export default function handler(req, res, next) {
-  async function getURI(url) {
-    try {
-      const response = await axios.get(url);
-      if (response.status !== 200) {
-        return res.status(response.status).json({ type: 'error', message: response.statusText });
-      } else {
-      res.json(JSON.stringify(response.data))
-      }
-    } catch (error) {
-      console.log(error.message, "ERR")
-      // return res.status(500).json({ type: 'error', message: error.message });
-    }
-  } 
+export default async function handler(req, res) {
+  // Устанавливаем заголовки CORS
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Content-Type', 'application/json');
-  getURI(JSON.parse(req.body)['my-url'])
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+
+  if (req.method === 'OPTIONS') {
+    // Обработка предварительного (OPTIONS) запроса
+    res.status(200).end();
+    return;
+  }
+
+  try {
+    const { 'my-url': myUrl } = JSON.parse(req.body);
+
+    const response = await axios.get(myUrl);
+
+    if (response.status !== 200) {
+      return res.status(response.status).json({ type: 'error', message: response.statusText });
+    }
+
+    // Отправляем данные в формате JSON
+    res.json(response.data);
+  } catch (error) {
+    console.error(error.message, "ERR");
+    res.status(500).json({ type: 'error', message: error.message });
+  }
 }
