a6879bf44cbd36d523839817142dbc955c71b0c2
Revenkov Egor <revenkov.egor@gmail.com>
2023-09-05 23:22:17 +0300

    Fix api v3

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,22 +1,5 @@
 import axios from 'axios';
 
-const allowCors = fn => async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
-  if (req.method === 'OPTIONS') {
-    res.status(200).end()
-    return
-  }
-  return await fn(req, res)
-}
-
 const handler = async (req, res) => {
   // Устанавливаем заголовки CORS
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -46,4 +29,4 @@ const handler = async (req, res) => {
   }
 }
 
-export default allowCors(handler)
+export default handler
