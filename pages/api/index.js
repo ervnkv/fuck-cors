6f9b26f95e476dfb50fac662355fed0867310d72
Егор Ревенков <revenkov.egor@gmail.com>
2023-09-07 09:53:00 +0300

    Fix api v6

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,32 +1,29 @@
-import axios from 'axios';
+import axios from 'axios'
 
 const handler = async (req, res) => {
   // Устанавливаем заголовки CORS
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS')
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
 
   if (req.method === 'OPTIONS') {
     // Обработка предварительного (OPTIONS) запроса
-    res.status(200).end();
-    return;
+    res.status(200).end()
+    return
   }
-
   try {
-    const { 'URL': myUrl } = JSON.parse(req.body);
+    const url = req.body.data.URL
 
-    const response = await axios.get(myUrl);
+    const response = await axios.get(url)
 
     if (response.status !== 200) {
-      return res.status(response.status).json({ type: 'error', message: response.statusText });
+      return res.status(response.status).json({ type: 'error', message: response.statusText })
     }
 
     // Отправляем данные в формате JSON
-    res.json(response.data);
+    res.json(response.data)
   } catch (error) {
-    console.error(error.message, "ERR");
-    res.status(500).json({ type: 'error', message: req.body });
-    // res.status(500).json({ type: 'error', message: error.message });
+    res.status(500).json({ type: 'error', message: req.body })
   }
 }
 
